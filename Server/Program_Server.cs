6a6f581f9e385e84b45992141be7193ae811c78c
Barnes, Patrick <zz-pbarnes@exchange.fullerton.edu>
2017-04-10 16:10:57 -0700

    seperated server loop thread, graceful close by user

--- Server/Program_Server.cs
+++ Server/Program_Server.cs
@@ -12,6 +12,7 @@ namespace Server
 {
     class Program_Server
     {
+        static bool serverQuit = false;
         static int portClients = 13000;
         static UdpClient udpClients = null;
         static IPEndPoint remoteIpClients = null;
@@ -20,6 +21,7 @@ namespace Server
         //static UdpClient udpGUIs = null;        //for later
         //static IPEndPoint remoteIpGUIs = null;  //for later
         //need a container for clients
+        static System.Object clients_lock = new System.Object(); //threadlocking
         static MessageServerGUI_Clients clients = new MessageServerGUI_Clients(MessageTypes.MSG_NEW); //has a list inside
         
         //need a container for GUI
@@ -38,62 +40,104 @@ namespace Server
             //IPEndPoint object will allow us to read datagrams sent from any source. ish
             remoteIpClients = new IPEndPoint(IPAddress.Any, portClients);
         }
+        static void StopServer()
+        {
+            udpClients.Close();
+        }
+        static void RunServer()
+        {
+            while (!serverQuit)
+            {
+                // Blocks until a message returns on this socket from a remote host.
+                Byte[] receiveBytes = udpClients.Receive(ref remoteIpClients);
+                // Sends a message to the host to which you have connected.
+
+                MemoryStream ms = new MemoryStream(receiveBytes);
+                MessageClientServer_Client msg = (MessageClientServer_Client)Serializer.Deserialize<MessageClientServer_Client>(ms);
+                // Uses the IPEndPoint object to determine which of these two hosts responded.
+                //msg received
+                //check serial against serials we have
+                //http://stackoverflow.com/questions/9854917/how-can-i-find-a-specific-element-in-a-listt
+
+                int idx = clients.client_list.FindIndex(x => x.machine_serial == msg.machine_serial);
+                Console.WriteLine("=====");
+                //Console.WriteLine("idx:" + idx);
+                if (idx < 0) //if new
+                {
+                    clients.client_list.Add(new ClientStatus(msg));
+                    if (msg.msgtype == MessageTypes.MSG_NEW)
+                        Console.WriteLine("New client - Started");
+                    else if (msg.msgtype == MessageTypes.MSG_UPDATEPUSH)
+                        Console.WriteLine("New client - reported");
+                    else
+                        Console.WriteLine("New client - ERRORS!!" + msg.msgtype);
+                }
+                else //if update
+                {
+                    ulong missed = clients.client_list[idx].Update(msg);
+                    if (msg.msgtype == MessageTypes.MSG_UPDATEPUSH)
+                        Console.WriteLine("Known client - reported" + " Lost:" + missed);
+                    else if (msg.msgtype == MessageTypes.MSG_NEW)
+                        Console.WriteLine("Known client - Started");
+                    else
+                        Console.WriteLine("Known client - ERRORS!!" + msg.msgtype + " Lost:" + missed);
+                }
+                Console.WriteLine(msg.label + " said " + msg.msg);
+                Console.WriteLine("Sent from " +
+                                            remoteIpClients.Address.ToString() +
+                                            " on their port:" +
+                                            remoteIpClients.Port.ToString());
+                //Console.WriteLine("=====objdump=====");
+                //msg.output();
+                //send back
+                Byte[] sendBytes = Encoding.ASCII.GetBytes("Hi " +
+                                            remoteIpClients.Address.ToString() +
+                                            " Got your messsage!");
+                udpClients.Send(sendBytes, sendBytes.Length, remoteIpClients);
+            }
+        }
+
         static void Main(string[] args)
         {
+            //ctrl+c trapping... doesnt work?
+            Console.CancelKeyPress += delegate {
+                // call methods to clean up
+                serverQuit = true;
+            };
             try
             {
+                Console.WriteLine("Opening Port...");
                 StartServer();
-                // Blocks until a message returns on this socket from a remote host.
+                System.Threading.Thread serverThread = new System.Threading.Thread(RunServer);
+                serverThread.IsBackground = false;
                 Console.WriteLine("Waiting for messages...");
-                while (true)
+                serverThread.Start();
+                //sit here for exit
+                char userinput = 'z';
+                while (!serverQuit)
                 {
-                    Byte[] receiveBytes = udpClients.Receive(ref remoteIpClients);
-                    // Sends a message to the host to which you have connected.
-                    
-                    MemoryStream ms = new MemoryStream(receiveBytes);
-                    MessageClientServer_Client msg = (MessageClientServer_Client) Serializer.Deserialize<MessageClientServer_Client>(ms);
-                    // Uses the IPEndPoint object to determine which of these two hosts responded.
-                    //msg received
-                    //check serial against serials we have
-                    //http://stackoverflow.com/questions/9854917/how-can-i-find-a-specific-element-in-a-listt
+                    Console.WriteLine("press q or CTRL+C to quit");
+                    userinput = Console.ReadKey().KeyChar; //true means dont echo
+                    Console.WriteLine();//if not echoing.... comment this out!
+                    //MENU HERE ...if ever
+                    switch (userinput)
+                    {
+                        case 'q':
+                            serverQuit = true;
+                            break;
+                    }
 
-                    int idx = clients.client_list.FindIndex(x => x.machine_serial == msg.machine_serial);
-                    Console.WriteLine("=====");
-                    //Console.WriteLine("idx:" + idx);
-                    if (idx < 0) //if new
-                    {
-                        clients.client_list.Add(new ClientStatus(msg));
-                        if (msg.msgtype == MessageTypes.MSG_NEW)
-                            Console.WriteLine("New client - Started");
-                        else if (msg.msgtype == MessageTypes.MSG_UPDATEPUSH)
-                            Console.WriteLine("New client - reported");
-                        else
-                            Console.WriteLine("New client - ERRORS!!" + msg.msgtype);
-                    }
-                    else //if update
-                    {
-                        ulong missed = clients.client_list[idx].Update(msg);
-                        if (msg.msgtype == MessageTypes.MSG_UPDATEPUSH)
-                            Console.WriteLine("Known client - reported" + " Lost:" + missed);
-                        else if (msg.msgtype == MessageTypes.MSG_NEW)
-                            Console.WriteLine("Known client - Started");
-                        else
-                            Console.WriteLine("Known client - ERRORS!!" + msg.msgtype + " Lost:" + missed);
-                    }
-                    Console.WriteLine(msg.label + " said " + msg.msg);
-                    Console.WriteLine("Sent from " +
-                                                remoteIpClients.Address.ToString() +
-                                                " on their port:" +
-                                                remoteIpClients.Port.ToString());
-                    //Console.WriteLine("=====objdump=====");
-                    //msg.output();
-                    //send back
-                    Byte[] sendBytes = Encoding.ASCII.GetBytes("Hi " +
-                                                remoteIpClients.Address.ToString() +
-                                                " Got your messsage!");
-                    udpClients.Send(sendBytes, sendBytes.Length, remoteIpClients);
                 }
-                
+                Console.WriteLine("Closing");
+                Console.Write("Waiting for port...");
+                serverThread.Join(1000); //wait 2 seconds in case it is mid process
+                if (serverThread.IsAlive)
+                {
+                    //more agressive kill, probably has no message processing anyways
+                    serverThread.Abort();
+                }
+                Console.Write("Ready to close...");
+                //StopServer(); // inside finally block
             }
             catch (Exception e)
             {
@@ -101,7 +145,8 @@ namespace Server
             }
             finally
             {
-                udpClients.Close();
+                StopServer();
+                Console.WriteLine("Closed");
             }
         }//end of main
     }
